d400c9849695e76c1016ec70925c955c84322e15
Jeff Guymon <jeffre@users.noreply.github.com>
2021-03-27 09:54:34 -0700

    clarified comment

--- 003-LargePrimeFactor/LargePrimeFactor.go
+++ 003-LargePrimeFactor/LargePrimeFactor.go
@@ -12,8 +12,8 @@ func PrimeFactors(n int) (primes []int) {
 
 	for i := 2; i <= maximum; i++ {
 
-		// Because the same integer can occur more than once as a prime factor
-		// we will repeat the test while deincrementing n
+		// Because the same prime can reoccur, keep testing until we have
+		// them all
 		for n%i == 0 {
 			primes = append(primes, i)
 			n /= i
