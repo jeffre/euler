fb7ee70e20bab608088163ee1b9647a8dd971c2f
jeffre <jeffre@users.noreply.github.com>
2021-03-27 23:54:04 -0700

    Added comments

--- 001-SumMultiples/sumMultiples.go
+++ 001-SumMultiples/sumMultiples.go
@@ -2,8 +2,14 @@ package euler001
 
 // SumMutiples sums all divisors less than max
 func SumMultiples(max int, divisors []int) (total int) {
+
+	// Check all integers less than max
 	for i := 1; i < max; i++ {
+
+		// Try each of the divisors. If any work, add it to the total and then
+		// break out of the divisors loop
 		for _, d := range divisors {
+
 			if i%d == 0 {
 				total += i
 				break
