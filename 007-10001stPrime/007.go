927bf4f5aa46d43c2d002a9e0ad9676579b290f9
jeffre <jeffre@users.noreply.github.com>
2021-03-31 13:47:27 -0700

    added comments

--- 007-10001stPrime/007.go
+++ 007-10001stPrime/007.go
@@ -2,20 +2,28 @@ package euler007
 
 import "strconv"
 
+// PrimeByPos retuns the nth prime number (sorted from smallest to largest)
 func PrimeByPos(pos int) int {
+
+	// Create a slice of booleans that represents integers from 0 to max. This
+	// will be used to map out if they are prime (false) or not prime (true).
 	max := 999999
 	p := make([]bool, max)
 
+	// 0 and 1 are not prime
 	p[0] = true
 	p[1] = true
-	for i := 2; i <= max; i++ {
 
+	// Find all values that are the product of two numbers (i and j) less than
+	// max. These values are not prime.
+	for i := 2; i <= max; i++ {
 		for j := 2; i*j < max; j++ {
 			p[i*j] = true
 		}
-
 	}
 
+	// All untouched booleans are prime. So we iterate over the array, making
+	// note of the position where the prime number was found.
 	primes := []int{2}
 	for id, val := range p {
 		if !val {
