c763d905a710c220e50388515d5f3f3347cacfd9
jeffre <jeffre@users.noreply.github.com>
2021-04-15 13:14:00 -0700

    count divisors using prime factors

--- 012-HighlyDivisibleTriangle/012.go
+++ 012-HighlyDivisibleTriangle/012.go
@@ -1,45 +1,74 @@
 package euler012
 
-func HighlyDivisibleTriangle(divisors int) (triangle int) {
+import euler007 "github.com/jeffre/euler/007-10001stPrime"
+
+// DivisorsCnt return the number of divisors for a given number n, provided a
+// slice of prime numbers. Refer to Euler7 for a prime number generator.
+func DivisorsCnt(n int, primes []int) (divisorcnt int) {
+
+	/*
+		Any integer N can be expressed as follows:
+			N = P₁^A₁ * P₂^A₂ * P₃^A₃ * …
+		where Pₙ is a prime number and Aₙ is its exponent.
+
+		For example 28 = 2² * 7¹
+
+		Furthermore, the number of divisors D(N) of any integer N can be computed from:
+		    D(N) = (A₁+1) * (A₂+1) * (A₃+1) * …
+		where Aₙ are the exponents of the distinct prime numbers which are factors of N
+
+		For example, the number of divisors of 28 would thus be:
+		    D(28) = (2+1) * (1+1) = 3 * 2 = 6
+	*/
+
+	// Starts at 1 because the calculation method uses multiplication
+	divisorcnt = 1
+
+	// Look for prime factors of `n`
+	for _, p := range primes {
+
+		// pfexponent (prime factor exponent) holds the exponential value of
+		// a distinct prime factor of `n`.
+		pfcnt := 0
+		for n%p == 0 {
+			n /= p
+			pfcnt += 1
+		}
+
+		// Multiply this prime factor (plus one) with any previous distinct
+		// prime factors
+		if pfcnt > 0 {
+			divisorcnt *= (pfcnt + 1)
+		}
+
+		// Test if all prime factors have been divided away
+		if n == 1 {
+			break
+		}
+	}
+	return
+}
+
+// HighlyDivisibleTriangle returns the first triangle number to have `n`
+// divisors.
+func HighlyDivisibleTriangle(n int) (triangle int) {
 
 	// Quickly handle these tricky divisors
-	switch divisors {
+	switch n {
 	case 0:
 		return 0
 	case 1:
 		return 1
 	}
 
+	// Generate a list of all primes less than 65500
+	primes := euler007.Primes(65500)
+
 	// Iterate through triangle numbers starting from 1
 	for i := 1; ; i++ {
 		triangle += i
-
-		// Get the divisors of the triangle number
-		var factors []int
-		max := triangle
-		for j := 1; j < max; j++ {
-
-			// if j is a divisor
-			if triangle%j == 0 {
-
-				// Append first factor
-				factors = append(factors, j)
-				remainder := triangle / j
-
-				// Reduce max
-				max = remainder
-
-				// Check if theres a unique second factor
-				if remainder != j {
-					factors = append(factors, max)
-				}
-
-				// Check if we've found desired triangle
-				if len(factors) >= divisors {
-					//fmt.Printf("%v: %+v\n", triangle, factors)
-					return triangle
-				}
-			}
+		if DivisorsCnt(triangle, primes) >= n {
+			return
 		}
 	}
 }
--- 012-HighlyDivisibleTriangle/012_test.go
+++ 012-HighlyDivisibleTriangle/012_test.go
@@ -3,7 +3,35 @@ package euler012
 import (
 	"fmt"
 	"testing"
+
+	euler7 "github.com/jeffre/euler/007-10001stPrime"
 )
+
+func TestDivisorsCnt(t *testing.T) {
+	cases := []struct {
+		num      int
+		divisors int
+	}{
+		{1, 1},
+		{2, 2},
+		{3, 2},
+		{4, 3},
+		{5, 2},
+		{28, 6},
+		{76576500, 576},
+	}
+
+	primes := euler7.Primes(65500)
+
+	for _, test := range cases {
+		t.Run(fmt.Sprintf("%v", test.num), func(t *testing.T) {
+			got := DivisorsCnt(test.num, primes)
+			if got != test.divisors {
+				t.Errorf("got %v want %v", got, test.divisors)
+			}
+		})
+	}
+}
 
 func TestHighlyDivisibleTriangle(t *testing.T) {
 	cases := []struct {
@@ -31,6 +59,6 @@ func TestHighlyDivisibleTriangle(t *testing.T) {
 
 func BenchmarkHighlyDivisibleTriangle(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		HighlyDivisibleTriangle(501)
+		HighlyDivisibleTriangle(500)
 	}
 }
