115e86e848edcdad25f228d128b71bcca1f371bd
jeffre <jeffre@users.noreply.github.com>
2021-04-21 11:40:05 -0700

    solve with prime factors

--- 005-SmallestMultiple/005.go
+++ 005-SmallestMultiple/005.go
@@ -1,8 +1,52 @@
 package euler5
 
-// SmallestMultiple find the smallest common multiple of integers that are less
-// than max
-func SmallestMultiple(max int) int {
+import (
+	"math"
+
+	euler7 "github.com/jeffre/euler/007-10001stPrime"
+)
+
+// SmallestMultiple finds the smallest common multiple of integers that are less
+// than max.
+func SmallestMultiple(max int) (sm int) {
+
+	// Smallest Multiple will be multiplied against other numbers so set
+	// default value to 1.
+	sm = 1
+
+	// Get all primes numbers that do not exceed max
+	primes := euler7.Primes(max)
+
+	// Iterate over the primes
+	for _, prime := range primes {
+
+		// Calculate the number of times this prime will occur as a factor within the
+		// sequence of 2 thru `max`.
+		exponent := int(math.Log(float64(max)) / math.Log(float64(prime)))
+
+		// Calculate prime^exponent and multiply this with the prior prime^exponents
+		sm *= int(math.Pow(float64(prime), float64(exponent)))
+	}
+
+	return sm
+}
+
+// Solve returns the answer for this Project Euler challenge
+func Solve() int {
+	return SmallestMultiple(20)
+}
+
+/*
+// Left for reference only. Using bruteforcing to solve this problem is slower
+// (by magnitudes) than the current method.
+
+// cpu: Intel(R) Core(TM) i7-4870HQ CPU @ 2.50GHz
+// BenchmarkSmallestMultipleBrute-8               5         202224960 ns/op
+// BenchmarkSmallestMultiple-8              2173063               544.4 ns/op
+
+// SmallestMultiple finds the smallest common multiple of integers that are less
+// than max.
+func SmallestMultipleBrute(max int) int {
 
 	// Iterate into infinity in increments of the largest divisor
 	for i := max; ; i += max {
@@ -23,8 +67,4 @@ func SmallestMultiple(max int) int {
 		}
 	}
 }
-
-// Solve returns the answer for this Project Euler challenge
-func Solve() int {
-	return SmallestMultiple(20)
-}
+*/
--- 005-SmallestMultiple/005_test.go
+++ 005-SmallestMultiple/005_test.go
@@ -31,3 +31,34 @@ func BenchmarkSmallestMultiple(b *testing.B) {
 		SmallestMultiple(max)
 	}
 }
+
+/*
+// This section is here for reference on how to test a solution by bruteforce.
+
+func TestSmallestMultipleBrute(t *testing.T) {
+	cases := []struct {
+		max  int
+		want int
+	}{
+		{10, 2520},
+		{20, 232792560},
+	}
+
+	for _, test := range cases {
+		t.Run(fmt.Sprintf("testing %v", test.max), func(t *testing.T) {
+			got := SmallestMultipleBrute(test.max)
+			if got != test.want {
+				t.Errorf("got %v want %v", got, test.want)
+			}
+		})
+
+	}
+}
+
+func BenchmarkSmallestMultipleBrute(b *testing.B) {
+	max := 20
+	for i := 0; i < b.N; i++ {
+		SmallestMultipleBrute(max)
+	}
+}
+*/
